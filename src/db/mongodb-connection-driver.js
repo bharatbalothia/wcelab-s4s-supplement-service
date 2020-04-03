274a0d38b1f8aad97160a6fff7e27647d9200b78
Akhil Pawar <apawar@us.ibm.com>
2020-04-03 17:10:58 -0500

    db driver updates

--- src/db/mongodb-connection-driver.js
+++ src/db/mongodb-connection-driver.js
@@ -12,19 +12,21 @@ var dbOptions = {
 }
 var dbEnvironment = process.env.DB_ENVIRONMENT || "NULL";
 
-if(process.env.DB_ENVIRONMENT == "DEV"){
+if(dbEnvironment == "DEV"){
     const host = process.env.MONGODB_HOST;
     const port = process.env.MONGODB_PORT;
     const db = process.env.MONGODB_DBNAME;
     uri = 'mongodb://' + host + ':' + port + '/' + db;
     console.log("uri: " + uri);
-}else if(process.env.DB_ENVIRONMENT == "IBM_CLOUD"){
+}else if(dbEnvironment == "IBM_CLOUD"){
     var cloudDBConnectionFile = fs.readFileSync('cloud_db_connection.json');
     process.env.VCAP_SERVICES = JSON.stringify(JSON.parse(cloudDBConnectionFile));
 }
 
 if (process.env.VCAP_SERVICES) {
-    process.env.DB_ENVIRONMENT = "DEFAULT";
+    if(dbEnvironment != "IBM_CLOUD"){
+        dbEnvironment = "DEFAULT";
+    }
     var env = JSON.parse(process.env.VCAP_SERVICES);
     uri = env["databases-for-mongodb"][0]["credentials"]["connection"]["mongodb"]["composed"][0];
     certFileBuf = base64Util.decode(env["databases-for-mongodb"][0]["credentials"]["connection"]["mongodb"]["certificate"]["certificate_base64"]);
@@ -35,8 +37,12 @@ if (process.env.VCAP_SERVICES) {
 
 console.log("DB_ENVIRONMENT: " + dbEnvironment);
 
-mongoose.connect(uri, dbOptions).then(() => {
-    console.log('DB Connection successful !!');
-}).catch(() => {
-    console.log('DB Connection FAILURE !!!');
-});
+if(dbEnvironment == "NULL"){
+    console.log('No DB connections details available to the server !!! Refer to .env_sample file for dev db connection.');
+}else{
+    mongoose.connect(uri, dbOptions).then(() => {
+        console.log('DB Connection successful !!');
+    }).catch(() => {
+        console.log('DB Connection FAILURE !!!');
+    });
+}
