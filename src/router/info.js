35c39c6fc3194dd422b24fcc88d935ddafef96bc
Akhil Pawar <apawar@us.ibm.com>
2020-04-04 13:06:18 -0500

    toolchain test

--- src/router/info.js
+++ src/router/info.js
@@ -7,7 +7,7 @@ router.get('/info', async (req, res) => {
         "project": "S4S - Sterling 4 Scarce Supply Inventory Visibility",
         "release": "Phase 1",
         "service": "s4s-supplement-service",
-        "version": "1.10",
+        "version": "1.11",
         "environment": "development"
     });
 });
