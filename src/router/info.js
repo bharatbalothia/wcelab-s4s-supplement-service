133f4028bdeef18ceb032f75e46d57c6ff071813
Akhil Pawar <apawar@us.ibm.com>
2020-05-06 18:12:51 -0500

    Enabled auth and updated version route

--- src/router/info.js
+++ src/router/info.js
@@ -2,15 +2,22 @@ const express = require('express');
 const router = new express.Router();
 const pjson = require('../../package.json');
 
-//Test for REST Endpoint access
 router.get('/', async (req, res) => {
-    res.send({
+    res.send(getAPIInfo());
+});
+
+router.get('/s4s', async (req, res) => {
+    res.send(getAPIInfo());
+});
+
+function getAPIInfo() {
+    return {
         "project": "S4S - Sterling 4 Scarce Supply Inventory Visibility",
         "release": "Phase 1",
         "service": "s4s-supplement-service",
         "version": pjson.version,
         "environment": process.env.APP_ENVIRONMENT
-    });
-});
+    };
+}
 
 module.exports = router;
