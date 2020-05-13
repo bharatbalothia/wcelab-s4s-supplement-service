02e803aedf38957c3f7e187ab5021dec7d9b1735
Ned Zhang <ned_zhang@us.ibm.com>
2020-05-12 23:04:09 -0500

    Updated the user supplier prod and connected supplier prod to array only

--- src/router/user-module.js
+++ src/router/user-module.js
@@ -10,33 +10,6 @@ module.exports = {
     // Get an user by user id
     getUserByUserId: async (tenantId, userId) => {
 
-        // var userFromDB = await User.findOne({ username: userId, tenant_id: tenantId });
-        // if (userFromDB == null) {
-        //     throw new ResourceNotFoundError(`tenant: ${tenantId} user: ${userId}`,
-        //         'Cannot find the specificed user in tenant.')
-        // }
-        // var user = JSON.parse(JSON.stringify(userFromDB));
-        // // var supplierList = new Set();
-        // var i = 0;
-        // if (user.buyers.length > 0) {
-
-        //     // console.log('** loading suppliers connected to user through its buyers **');
-
-        //     user.connected_suppliers = [];
-
-        //     buyerObjectList = await Buyer.find({ tenant_id: tenantId, buyer_id: {$in: user.buyers } })
-
-        //     buyerObjectList.forEach(buyerObj => {
-        //         if (buyerObj && buyerObj.suppliers && buyerObj.suppliers.length) {
-        //             buyerObj.suppliers.forEach(supplier => {
-        //                 // console.log(`pushing ${supplier} into ${userId} connected_suppliers list`);
-        //                 user.connected_suppliers.push(supplier)
-        //             })
-        //         }
-        //     })
-        // }
-        // // console.log(`** returning user object for ${userId} now. \n ${JSON.stringify(user)} **`);
-        
         return await getUserDetail(tenantId, userId)
 
     },
@@ -51,13 +24,13 @@ module.exports = {
             
             // console.log(`user ${userId} can see the products:  ${JSON.stringify(connected_supplier_products)}`);
 
-            user.connected_supplier_products = connected_supplier_products
+            return connected_supplier_products
 
             // console.log(`get user with connected supplier products: ${JSON.stringify(user)}`);
             
+        } else {
+            return undefined
         }
-
-        return user
     },
 
     getUserSupplierProductList: async (tenantId, userId) => {
@@ -70,13 +43,13 @@ module.exports = {
             
             console.log(`[getUserSupplierProductList] user ${userId} can see the products:  ${JSON.stringify(ownProductList)}`);
 
-            user.supplier_products = ownProductList
+            return ownProductList
 
             // console.log(`get user with connected supplier products: ${JSON.stringify(user)}`);
             
+        } else {
+            return undefined
         }
-
-        return user
     }
 }
 
